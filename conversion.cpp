32f27a468e7247cccaae29fa4a152347e2eef431
Andreas Duong <andreasduong1@gmail.com>
2025-10-29 15:55:09 +0700

    ver 2

--- conversion.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-using namespace std;
-
-int main() {
-    double x = (int) 3.14;
-
-    cout << x << endl;
-}
--- tempCodeRunnerFile.cpp
+++
@@ -1,2 +0,0 @@
- // cout << "Học sinh cuối cùng là: \n";
-    // danhSach[soLuong - 1] << cout ;
